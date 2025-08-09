749746fd80358a29e301d793986676f4c3631adb
Richard Lin <richardlin@Richards-MacBook-Pro.local>
2025-08-08 19:05:40 -0500

    connect image upload to utils

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 import streamlit as st
+import utils
 
 st.title("♻️ RecycleAI")
 st.write("Upload a picture of trash and we'll tell you how to dispose of it!")
-st.file_uploader("Upload your trash here!")
+img_file = st.file_uploader("Upload your trash here!", type=["png", "jpeg", "jpg"])
+if img_file is not None:
+    try: 
+            img_processed = utils.load_image_for_user(img_file)
+            st.image(img_processed, use_container_width=True)
+            st.write("Image loaded Successfully!")
+    except Exception:
+        st.write("Image did not loaded successfully.")
+    
+
+
 
 
