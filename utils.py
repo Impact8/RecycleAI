f38ea1f3968f1ded439b40de9d27172b1df1f950
Richard Lin <richardlin@Richards-MacBook-Pro.local>
2025-08-10 03:31:04 -0500

    some files wasn't saved properly this is to save them

--- utils.py
+++ utils.py
@@ -7,3 +7,15 @@ def load_image_for_user(file):
     img = Image.open(file)
     rgb_img = img.convert("RGBA")
     return rgb_img
+
+def load_image_for_model(file):
+    size = 224, 224
+    img1 = Image.open(file)
+    conv_rgb = img1.convert("RGB")
+    fully_proc_image = conv_rgb.resize(size)
+
+    transform = transforms.ToTensor()
+
+    return fully_proc_image
+
+
