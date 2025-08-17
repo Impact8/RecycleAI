d200f4700feb12648de035ceb607eb0f637c827a
Richard Lin <richardlin@Richards-MacBook-Pro.local>
2025-08-16 20:08:52 -0500

    Add the try/except to app.py

--- app.py
+++ app.py
@@ -9,17 +9,19 @@ device = get_device()
 model = build_model()
 img_file = st.file_uploader("Upload your trash here!", type=["png", "jpeg", "jpg"])
 if img_file is not None:
-
-    img_processed = utils.load_image_for_user(img_file)
-    st.image(img_processed, use_container_width=True)
-    with torch.no_grad():
-        img_tensor = utils.load_image_for_model(img_file)
-        model.to(device)
-        outputs = model(img_tensor.to(device))
-        predicted_class = outputs.argmax(dim=1).item()
-        st.write("Predicted class index:", predicted_class)
-        st.write("Predicted label:", classes[predicted_class])
-            
+    try:
+        img_processed = utils.load_image_for_user(img_file)
+        st.image(img_processed, use_container_width=True)
+        with torch.no_grad():
+            img_tensor = utils.load_image_for_model(img_file)
+            model.to(device)
+            outputs = model(img_tensor.to(device))
+            predicted_class = outputs.argmax(dim=1).item()
+            st.write("Predicted class index:", predicted_class)
+            st.write("Predicted label:", classes[predicted_class])
+    except Exception as e:
+        st.exception(e) 
+        st.write("Type of Error:", type(e).__name__)
     
 
 
